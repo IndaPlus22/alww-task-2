e1ee62dc208f88b4cc4886204e4e9f6c64ebd38b
Trollkaaar <albin.woxnerud@gmail.com>
2022-09-13 18:45:16 +0200

    idk man i give up

--- kattis_cyber-clara/src/main.rs
+++ kattis_cyber-clara/src/main.rs
@@ -1,20 +1,26 @@
 use std::collections::HashSet;
-use std::{
-    convert::TryInto,
-    io::{self, BufRead},
-};
+use std::io::{self, BufRead};
 
 fn main() {
     let (n, parsed_input) = parse_input(read_user_input());
-    let unique_names: HashSet<_> = parsed_input.iter().cloned().collect();
-    println!("{}", unique_number_of_guests(n, unique_names))
+    // let (n, parsed_input) = correct_way();
+    let tmp: usize = n.into();
+    let mut persons = Vec::<(String, String)>::new();
+    for x in 0..n {
+        let x_to_usize: usize = x.into();
+        persons.push((
+            parsed_input.get(x_to_usize).unwrap().to_string(),
+            parsed_input.get(x_to_usize + tmp).unwrap().to_string(),
+        ));
+    }
+    let unique_persons: HashSet<_> = persons.iter().clone().collect();
+    println!("{:?}", unique_persons.len());
 }
 fn read_user_input() -> String {
     //https://www.becomebetterprogrammer.com/rust-read-user-input-stdin/
     let mut user_input = String::new();
     let input = io::stdin();
     let mut lines = input.lock().lines();
-
     while let Some(line) = lines.next() {
         let input = line.unwrap();
 
@@ -30,23 +36,19 @@ fn read_user_input() -> String {
 fn parse_input(input: String) -> (u8, Vec<String>) {
     let split_input: Vec<&str> = input.trim().split_whitespace().collect();
     let (number, names) = split_input.split_at(1);
-    let names = names.to_vec().into_iter().map(|x| x.to_owned()).collect();
     let number: u8 = number[0].parse().expect("Error while parsing input!");
+    let names = names
+        .to_vec()
+        .into_iter()
+        .map(|x| x.to_owned())
+        .collect::<Vec<String>>();
     (number, names)
 }
 
-fn unique_number_of_guests(n: u8, unorganized_names: HashSet<String>) -> u8 {
-    let number_of_unorganized_names: u8 = unorganized_names.len().try_into().unwrap();
-    if is_even(number_of_unorganized_names.into()) {
-        (n * 2) - number_of_unorganized_names
-    } else {
-        (n * 2) - number_of_unorganized_names + 1
-    }
-}
-fn is_even(input: usize) -> bool {
-    if input % 2 == 0 {
-        true
-    } else {
-        false
-    }
+fn correct_way() -> (u8, Vec<String>) {
+    let input = io::stdin();
+    let mut lines = input.lock().lines().map(|var| var.ok().unwrap());
+    let number = lines.next().unwrap().parse::<u8>().unwrap();
+    let names = lines.collect::<Vec<String>>();
+    (number, names)
 }
